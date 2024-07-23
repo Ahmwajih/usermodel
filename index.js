ea291723bcf5311cd41dc2ebe607096b1de92046
ahmwajih <ahmed.wajih.ben.smida@gmail.com>
2024-07-23 19:42:31 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const { configDotenv } = require("dotenv");
+const express = require("express");
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT;
+
+app.use(express.json());
+
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
